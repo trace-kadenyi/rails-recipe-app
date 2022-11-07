c3a0bd5073764e2e746be57221da36052ece78ad
kadenyi <treykadenyi@gmail.com>
2022-11-07 21:22:03 +0300

    Add food and user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :foods
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
