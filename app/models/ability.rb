7bdf7f3539554c4ecf9aef81c1c843924bfd8f42
kadenyi <treykadenyi@gmail.com>
2022-11-10 15:32:24 +0300

    Add authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,18 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    can :read, Recipe do |recipe|
+      recipe.public
+    end
+    can :manage, Recipe do |recipe|
+      recipe.user == user
+    end
+    can :read, Food
+    can :manage, Food do |food|
+      food.user == user
+    end
+  end
+
 end
